83875808039ff5e2cab1d198f4990603b3d99f4e
mohiteabhi <mohiteabhijeet74@gmail.com>
2025-06-15 00:10:29 +0530

    CRUD API for user_info

--- src/main/java/com/example/volunteerapp/repository/UserRepository.java
+++ src/main/java/com/example/volunteerapp/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.volunteerapp.repository;
 import com.example.volunteerapp.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserInfo, Long> {
     Optional<UserInfo> findByEmail(String email);
     boolean existsByEmail(String email);
